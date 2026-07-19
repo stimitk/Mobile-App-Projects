271c8fb6cb3e129978fd1d2a5958b09115e6b484
Stimit Keluskar <144183149+stimit1404k@users.noreply.github.com>
2026-07-19 17:20:10 +0530

    Learning Flutter + SupaBase, completed Lec 03

--- Flutter + SupaBase/flutter_application_1/lib/auth/login_screen.dart
+++ Flutter + SupaBase/flutter_application_1/lib/auth/login_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/auth/register_screen.dart';
 import 'package:flutter_application_1/home/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -43,6 +47,47 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
+  continueWithGoogle() async {
+    try {
+      GoogleSignIn googleSignIn = GoogleSignIn.instance;
+      await googleSignIn.initialize(
+        serverClientId: dotenv.env['WEB_CLIENT']!,
+        clientId: Platform.isAndroid
+            ? dotenv.env['ANDROID_CLIENT']!
+            : dotenv.env['IOS_CLIENT']!,
+      );
+      GoogleSignInAccount googleSignInAccount = await googleSignIn
+          .authenticate();
+
+      String idToken = googleSignInAccount.authentication.idToken ?? '';
+      final authorization =
+          await googleSignInAccount.authorizationClient.authorizationForScopes([
+            'email',
+            'profile',
+          ]) ??
+          await googleSignInAccount.authorizationClient.authorizeScopes([
+            'email',
+            'profile',
+          ]);
+
+      final result = await supabase.auth.signInWithIdToken(
+        provider: OAuthProvider.google,
+        idToken: idToken,
+        accessToken: authorization.accessToken,
+      );
+
+      if (result.user != null && result.session != null) {
+        Navigator.pushAndRemoveUntil(
+          context,
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+          (context) => false,
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,6 +114,20 @@ class _LoginScreenState extends State<LoginScreen> {
                   },
                   child: Text('Login'),
                 ),
+          SizedBox(height: 16),
+          GestureDetector(
+            onTap: () {
+              continueWithGoogle();
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/icons/google.png', height: 30),
+                SizedBox(width: 16),
+                Text('Signin with Google'),
+              ],
+            ),
+          ),
           SizedBox(height: 16),
           TextButton(
             onPressed: () {
